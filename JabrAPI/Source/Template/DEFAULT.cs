999334e06bc936a74ebf861ce51e007cb65520cb
Gyro <gyroscopic.why@gmail.com>
2026-07-29 03:26:31 +0300

    -Text & -TextToBinary duplicates
    136 -> 131 -> 130 -> 126 (kb)
    
    And now:
    126 -> 59 (kb)

--- JabrAPI/Source/Template/DEFAULT.cs
+++ JabrAPI/Source/Template/DEFAULT.cs
@@ -6,25 +6,6 @@ namespace JabrAPI
 {
     static internal class DEFAULT
     {
-        static public class CHARACTERS
-        { 
-            static private readonly string _withoutSpace =
-                "`1234567890" +
-                "abcdefghijklmnopqrstuvwxyz" +
-                "-=[]\\;',./~!@#$%^&*()_+" +
-                "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-                "{}|:\"<>?№" +
-                "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" +
-                "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-            
-            
-
-            static public string WITHOUT_WSPACE => _withoutSpace;
-            static public string WITH_WSPACE    => _withoutSpace + " \r\t\n";
-        }
-
-
-
         static public readonly Byte[] BYTES = [ 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
                                                 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
                                                 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F,
