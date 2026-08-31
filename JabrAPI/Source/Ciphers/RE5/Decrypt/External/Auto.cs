9ac54b591d37293578c44153477f20b596c633f3
Gyro <gyroscopic.why@gmail.com>
2026-09-01 00:21:35 +0300

    v5.6.3.1: -Duplicate overloads of Decrypt(out Exception? exception)

--- JabrAPI/Source/Ciphers/RE5/Decrypt/External/Auto.cs
+++ JabrAPI/Source/Ciphers/RE5/Decrypt/External/Auto.cs
@@ -44,32 +44,6 @@ namespace JabrAPI
                 /// <param name="mode">Alias to chosen overload</param>
                 /// <param name="encrypted">Obfuscated data</param>
                 /// <param name="reKey">RE5 Encryption key for denoising and deciphering</param>
-                /// <param name="exception"><see cref="System.Exception"/> if something fails</param>
-                static public List<Byte> Data(DataDecryptionMode mode, List<Byte> encrypted, ReKey reKey, out Exception? exception)
-                {
-                    exception = null;
-                    return mode switch
-                    {
-                        DataDecryptionMode.SAFE => RE5.Decrypt.Data(encrypted, reKey, out exception),
-                        DataDecryptionMode.FAST => RE5.Decrypt.Fast.Data(encrypted, reKey),
-
-                        DataDecryptionMode.SAFE_WITH_NOISE_ADDITION
-                            => RE5.Decrypt.WithNoiseRemoval.Data(encrypted, reKey, out exception),
-                        DataDecryptionMode.FAST_WITH_NOISE_ADDITION
-                            => RE5.Decrypt.FastWithNoiseRemoval.Data(encrypted, reKey),
-                        _ => []
-                    };
-                }
-
-                /// <summary>
-                /// Returns the <b>Decrypted</b> data of <paramref name="encrypted"/>
-                /// </summary>
-                /// <returns><b>Decrypted</b> data of <paramref name="encrypted"/></returns>
-                /// 
-                /// <param name="mode">Alias to chosen overload</param>
-                /// <param name="encrypted">Obfuscated data</param>
-                /// <param name="reKey">RE5 Encryption key for denoising and deciphering</param>
-                /// <param name="throwExceptions"></param>
                 static public List<Byte> Data(DataDecryptionMode mode, List<Byte> encrypted, ReKey reKey, bool throwExceptions = false)
                 {
                     return mode switch
@@ -81,11 +55,9 @@ namespace JabrAPI
                             => RE5.Decrypt.WithNoiseRemoval.Data(encrypted, reKey, throwExceptions),
                         DataDecryptionMode.FAST_WITH_NOISE_ADDITION
                             => RE5.Decrypt.FastWithNoiseRemoval.Data(encrypted, reKey),
-                        _ => []
+                          _ => []
                     };
                 }
-
-
 
 
 
@@ -104,47 +76,6 @@ namespace JabrAPI
                 /// <param name="fileName">Original FILE NAME</param>
                 /// <param name="absoluteOutputDirectory">Path where the temporary and output FILE will be stored</param>
                 /// <param name="reKey">RE5 Encryption key for denoising and deciphering</param>
-                /// <param name="exception"><see cref="System.Exception"/> if something fails</param>
-                /// <param name="deleteTempFileAfterUse">Whether the Temporary FILE will be deleted at the end</param>
-                static public void File(DataDecryptionMode mode, string absoluteInputDirectory, string fileName,
-                    string absoluteOutputDirectory, ReKey reKey, out Exception? exception, bool deleteTempFileAfterUse = true)
-                {
-                    exception = null;
-                    switch (mode)
-                    {
-                        case DataDecryptionMode.SAFE:
-                            RE5.Decrypt.File(absoluteInputDirectory, fileName,
-                            absoluteOutputDirectory, reKey, out exception); break;
-                        case DataDecryptionMode.FAST:
-                            RE5.Decrypt.Fast.File(absoluteInputDirectory, fileName,
-                            absoluteOutputDirectory, reKey); break;
-
-                        case DataDecryptionMode.SAFE_WITH_NOISE_ADDITION:
-                            //RE5.Decrypt.WithNoiseRemoval.File(absoluteInputDirectory, fileName, absoluteOutputDirectory, reKey, out exception);
-                            break;
-                        case DataDecryptionMode.FAST_WITH_NOISE_ADDITION:
-                            RE5.Decrypt.FastWithNoiseRemoval.File(absoluteInputDirectory, fileName,
-                            absoluteOutputDirectory, reKey); break;
-                        default: break;
-                    };
-                }
-
-                /// <summary>
-                /// Creates a <b>FILE</b> containing the <b>Decrypted</b> content<br/><br/>
-                ///   
-                /// <i>A temporary FILE is used for storing the Denoised content (if Denoising is selected)</i><br/>
-                /// It will be <b>deleted</b> in the end <b>based on <paramref name="deleteTempFileAfterUse"/></b><br/><br/>
-                /// 
-                /// Returns the <i>NAME</i> of the new <b>Decrypted <i>FILE</i></b>
-                /// </summary>
-                /// <returns>The <i>NAME</i> of the new <b>Decrypted <i>FILE</i></b></returns>
-                /// 
-                /// <param name="mode">Alias to chosen overload</param>
-                /// <param name="absoluteInputDirectory">PATH to the original FILE</param>
-                /// <param name="fileName">Original FILE NAME</param>
-                /// <param name="absoluteOutputDirectory">Path where the temporary and output FILE will be stored</param>
-                /// <param name="reKey">RE5 Encryption key for denoising and deciphering</param>
-                /// <param name="throwExceptions"></param>
                 /// <param name="deleteTempFileAfterUse">Whether the Temporary FILE will be deleted at the end</param>
                 static public void File(DataDecryptionMode mode, string absoluteInputDirectory, string fileName,
                     string absoluteOutputDirectory, ReKey reKey, bool throwExceptions = false, bool deleteTempFileAfterUse = true)
---
+++ JabrAPI/Source/Ciphers/RE5/Decrypt/External/Safe.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+using static JabrAPI.Miscellaneous;
+
+
+
+namespace JabrAPI
+{
+    static public partial class RE5
+    {
+        static public partial class Decrypt
+        {
+            /// <summary>
+            /// Returns the <b>Decrypted</b> data of <paramref name="encrypted"/>
+            /// </summary>
+            /// <returns><b>Decrypted</b> data of <paramref name="encrypted"/></returns>
+            /// 
+            /// <param name="encrypted">Obfuscated data</param>
+            /// <param name="reKey">RE5 Encryption key for deciphering</param>
+            static public List<Byte> Data(List<Byte> encrypted, ReKey reKey, bool throwExceptions = false)
+            {
+                if (IsMessageAndReKeyValid(encrypted, reKey, throwExceptions) &&
+                    reKey.IsValid.ForDecryption(encrypted, throwExceptions))
+                {
+                    try
+                    {
+                        return RE5.Decrypt.Fast.Data(encrypted, reKey);
+                    }
+                    catch { throw; }
+                }
+                return [];
+            }
+
+
+
+            /// <summary>
+            /// Creates a <b>FILE</b> containing the <b>Decrypted</b> content<br/>
+            /// Returns the <i>NAME</i> of the new <b>Decrypted <i>FILE</i></b>
+            /// </summary>
+            /// <returns>The <i>NAME</i> of the new <b>Decrypted <i>FILE</i></b></returns>
+            /// 
+            /// <param name="absoluteInputDirectory">PATH to the original FILE</param>
+            /// <param name="fileName">Original FILE NAME</param>
+            /// <param name="absoluteOutputDirectory">Path where the temporary and output FILE will be stored</param>
+            /// <param name="reKey">RE5 Encryption key for deciphering</param>
+            static public (bool didSucceed, string resultFileName) File(string absoluteInputDirectory, string fileName,
+                string absoluteOutputDirectory, ReKey reKey, bool throwExceptions = false)
+            {
+                if (IsReKeyValid(reKey, throwExceptions) &&
+                    IsNoisifierValid(reKey.Noisifier, throwExceptions))
+                {
+                    try
+                    {
+                        return (true, RE5.Decrypt.Fast.File(absoluteInputDirectory, fileName, absoluteOutputDirectory, reKey));
+                    }
+                    catch { throw; }
+                }
+                return (false, "");
+            }
+        }
+    }
+}
